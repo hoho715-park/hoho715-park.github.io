431931b14c2b660c695c45c25fe372e42d61a112
hoho715 <andy2001@g.hongik.ac.kr>
2025-07-09 12:20:48 +0900

    feat : contact page 빈칸 채우기 기능 추가

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 function openModal() {
-      document.getElementById("modal-bg").style.display = "flex";
-    }
-    function closeModal(e) {
-      if (e.target.id === "modal-bg") {
-        document.getElementById("modal-bg").style.display = "none";
-      }
-    }
+  document.getElementById("modal-bg").style.display = "flex";
+}
+
+function closeModal(e) {
+  if (e.target.id === "modal-bg") {
+    document.getElementById("modal-bg").style.display = "none";
+  }
+}
+
+function submitForm() {
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+
+  if (!name) {
+    alert("name을 입력해주세요!");
+    return;
+  }
+  if (!email) {
+    alert("email을 입력해주세요!");
+    return;
+  }
+  if (!email.includes("@")) {
+    alert("이메일의 형식에 맞춰서 작성해주세요!");
+    return;
+  }
+  if (!message) {
+    alert("message를 입력해주세요!");
+    return;
+  }
+
+  alert("성공적으로 메일이 전송되었습니다.");
+  document.getElementById("modal-bg").style.display = "none";
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("message").value = "";
+}
